82b9368918de38b6428f83c94815858c2c714402
Yury Shchegolev <cardiffc@yandex.ru>
2020-02-16 16:08:16 +0300

    new commit

--- maven_sample/build_code.groovy
+++ maven_sample/build_code.groovy
@@ -1,9 +1,9 @@
 job('Build Code DSL') {
 	description('Build code for maven tomcat in new pipeline')
 	logRotator {
-		artifactDaysToKeep(5)
-        	artifactNumToKeep(5)
+		numToKeep(5)
 	}
+
 	
 	scm() {
 		git('https://github.com/cardiffc/Jenkins_Upgradev3.git','master')
